497eaec28f1126e4bae853b1773e65fb3972d4c8
Pyame <matteb504@gmail.com>
2023-09-23 19:17:19 +0200

    text to speach engine init, speak function created

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import speech_recognition as sr
+import pyttsx3
 
 
+# Speach engine init
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[12].id) # 12 - is for male english
+
+
+# Set activation word
+activationWord = "Alex" # It should be one word
+
+
+# Speach Regonition process
 recognizer = sr.Recognizer()
+
+
+# Define speak function
+def speak(text, rate = 120):
+    engine.setProperty('rate', rate)
+    engine.say(text)
+    engine.runAndWait()
+
 
 with sr.Microphone() as source:
     print("Say something!")
-    query = recognizer.listen(source)
+    audio = recognizer.listen(source)
+
     
 try:
-    print("You said: " + recognizer.recognize_whisper(query, language= "english"))
+    query = recognizer.recognize_whisper(audio, language= "english")
+    print("You said: " + query)
+    speak(query)
 except sr.UnknownValueError:
     print("Whisper could not understand audio")
 except sr.RequestError as e:
